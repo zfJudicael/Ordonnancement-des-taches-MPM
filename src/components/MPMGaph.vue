c2435d7d719251e788eda04393629d8800d8dda9
Fanny Judicaël <fannyjudicael501@gmail.com>
2026-03-07 22:17:29 +0300

    feat: add graph's download in PNG format feature

--- src/components/MPMGaph.vue
+++ src/components/MPMGaph.vue
@@ -2,38 +2,43 @@
     <div class="mpmGraph">
         <h3 style="text-align: center;">MPM Graph</h3>
 
-        <VueFlow :nodes="nodes" :edges="edges" :style="{border: '2px solid gray', overflow: 'hidden', height: '450px'}">
-            <template #node-start="startNodeProps">
-                <StartNode v-bind="startNodeProps" />
-            </template>
-            
-            <template #node-step="stepNodeProps">
-                <StepNode v-bind="stepNodeProps" />
-            </template>
-
-            <template #node-end="endNodeProps">
-                <EndNode v-bind="endNodeProps" />
-            </template>
-            
-            <template #edge-default="defaultEdgeProps">
-                <DefaultEdge v-bind="defaultEdgeProps" />
-            </template>
-            <Background />
-
-            <Controls />
-        </VueFlow>
+        <button @click="download">Télécharger</button>
+        <div ref="flowRef" :style="{border: '2px solid gray', overflow: 'hidden', height: '450px'}">
+            <VueFlow :nodes="nodes" :edges="edges" :style="{ background: 'white' }">
+                <template #node-start="startNodeProps">
+                    <StartNode v-bind="startNodeProps" />
+                </template>
+                
+                <template #node-step="stepNodeProps">
+                    <StepNode v-bind="stepNodeProps" />
+                </template>
+    
+                <template #node-end="endNodeProps">
+                    <EndNode v-bind="endNodeProps" />
+                </template>
+                
+                <template #edge-default="defaultEdgeProps">
+                    <DefaultEdge v-bind="defaultEdgeProps" />
+                </template>
+                <Background />
+    
+                <Controls />
+            </VueFlow>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import type { Node, Edge } from '@vue-flow/core';
 import { VueFlow } from '@vue-flow/core';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 import { Controls } from '@vue-flow/controls';
 import { Background } from '@vue-flow/background';
 import StartNode from './vue_flow/StartNode.vue';
 import StepNode from './vue_flow/StepNode.vue';
 import DefaultEdge from './vue_flow/DefaultEdge.vue';
 import EndNode from './vue_flow/EndNode.vue';
+import { ref } from 'vue';
 
 defineProps({
   nodes: {
@@ -46,6 +51,18 @@ defineProps({
   }
 })
 
+const flowRef = ref<HTMLElement>()
+const download = async ()=>{
+
+    if(flowRef.value){
+        const dataURL = await toPng(flowRef.value)
+
+        const link = document.createElement("a")
+        link.download = "diagramMPM.png"
+        link.href = dataURL
+        link.click()
+    }
+}
 </script>
 
 <style>
