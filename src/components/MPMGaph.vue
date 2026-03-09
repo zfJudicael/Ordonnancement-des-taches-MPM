1bbc231a6ae2db025ac2f3569749651f221f935c
Fanny Judicaël <fannyjudicael501@gmail.com>
2026-03-09 22:13:31 +0300

    feat<VueFlow>: enable exporting graph as JPEG

--- src/components/MPMGaph.vue
+++ src/components/MPMGaph.vue
@@ -2,7 +2,22 @@
     <div class="mpmGraph">
         <h3 style="text-align: center;">MPM Graph</h3>
 
-        <div ref="flowRef" :style="{border: '2px solid gray', overflow: 'hidden', height: '500px'}">
+        <div ref="flowRef" :style="{border: '2px solid gray', overflow: 'hidden', height: '500px', position: 'relative'}">
+            <div id="toolbar">
+                <div class="dropdown_button">
+                    <div class="menu_item">
+                        <i class="pi pi-download"></i>
+
+                        <ul class="dropdown">
+                            <li style="font-size: small;">Format :</li>
+                            <li class="dropdown-item">Pdf</li>
+                            <li class="dropdown-item" @click="downloadDiagram('png')">Png</li>
+                            <li class="dropdown-item" @click="downloadDiagram('jpeg')">Jpeg</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
             <VueFlow :nodes="nodes" :edges="edges" :style="{ background: 'white' }">
                 <template #node-start="startNodeProps">
                     <StartNode v-bind="startNodeProps" />
@@ -22,37 +37,12 @@
                 <Background />
     
                 <Controls>
-                    <ControlButton title="Download" @click="isDownloadDialogVisible = true">
-                        <i class="pi pi-download"></i>
-                    </ControlButton>
-
                     <ControlButton title="FullScreen" @click="toggleFullScreen">
                         <i class="pi pi-window-maximize"></i>
                     </ControlButton>
                 </Controls>
             </VueFlow>
         </div>
-
-        <Dialog v-model:visible="isDownloadDialogVisible" modal close-on-escape header='aa' :style="{ width: '20rem' }" :append-to="flowRef">
-            <form @submit.prevent="">
-                <div>
-                    <p style="margin-bottom: 0;">Durée</p>
-                    <!-- <InputNumber v-model="selectedTask.duration" name="duration" fluid/> -->
-                </div>
-
-                <!-- <div>
-                    <p style="margin-bottom: 0;">Tâches antérieures</p>
-                    <MultiSelect v-model="selectedTask.previousTasks" :options="selectedTable?.getTaskArray(selectedTaskKey)" display="chip" filter placeholder="Selectionner" style="width: 80%;" />
-                </div> -->
-
-                <Divider />
-
-                <div style="display: flex; gap: 5px; justify-content: center;">
-                    <Button type="submit" icon="pi pi-check" severity="info" raised label="Confirmer"/>
-                    <Button icon="pi pi-times" severity="danger" raised label="Annuler" @click="isDownloadDialogVisible = false"/>
-                </div>
-            </form>
-        </Dialog>
     </div>
 </template>
 
@@ -60,14 +50,13 @@
 import { ref } from 'vue';
 import type { Node, Edge } from '@vue-flow/core';
 import { VueFlow } from '@vue-flow/core';
-import { domToPng } from 'modern-screenshot';
+import { domToJpeg, domToPng } from 'modern-screenshot';
 import { Controls, ControlButton } from '@vue-flow/controls';
 import { Background } from '@vue-flow/background';
 import StartNode from './vue_flow/StartNode.vue';
 import StepNode from './vue_flow/StepNode.vue';
 import DefaultEdge from './vue_flow/DefaultEdge.vue';
 import EndNode from './vue_flow/EndNode.vue';
-import { Dialog, Button } from 'primevue';
 
 defineProps({
   nodes: {
@@ -80,18 +69,30 @@ defineProps({
   }
 })
 
-const isDownloadDialogVisible = ref<boolean>(false)
-
 const flowRef = ref<HTMLElement>()
-const download = async ()=>{
+const filter = (node: globalThis.Node)=>{
+    if (node instanceof HTMLElement && node.id === 'toolbar') return false;
 
+    return true;
+}
+const downloadDiagram = async (documentFormat: 'jpeg' | 'png')=>{
     if(flowRef.value){
-        const dataURL = await domToPng(flowRef.value, {
-            scale: 2
-        })
+        let dataURL = '';
 
-        const link = document.createElement("a")
-        link.download = "diagramMPM.png"
+        if(documentFormat == 'jpeg'){
+            dataURL = await domToJpeg(flowRef.value, {
+                scale: 2,
+                filter: filter
+            })
+        }else{
+            dataURL = await domToPng(flowRef.value, {
+                scale: 2,
+                filter: filter
+            })
+        }
+
+        const link = document.createElement("a");
+        link.download = `diagram.${documentFormat}`
         link.href = dataURL
         link.click()
     }
@@ -108,8 +109,54 @@ const toggleFullScreen = async ()=>{
 }
 </script>
 
-<style>
+<style lang="scss">
 @import '@vue-flow/core/dist/style.css';
 @import '@vue-flow/core/dist/theme-default.css';
 @import '@vue-flow/controls/dist/style.css';
+
+#toolbar{
+    border: 0.5px solid grey; 
+    position: absolute; 
+    top: 0; 
+    right: 0; 
+    z-index: 1; 
+    padding: 5px; 
+    margin: 5px; 
+    border-radius: 4px;
+
+    .dropdown_button{
+        position: relative;
+        
+        .menu_item{
+            width: fit-content;
+            cursor: pointer;
+            
+            .dropdown{
+                overflow: hidden;
+                max-height: 0;
+                position: absolute;
+                top: 60%;
+                right: 0;
+                list-style: none;
+                padding: 0;
+                transition: max-height 0.5s ease-in-out;
+        
+                .dropdown-item{
+                    padding: 5px 20px;
+                    background-color: rgb(236, 236, 236);
+    
+                    &:hover{
+                        background-color: rgb(182, 182, 182);
+                    }
+                }
+            }
+        
+            &:hover{
+                .dropdown{
+                    max-height: 300px;
+                }
+            }
+        }
+    }
+}
 </style>
