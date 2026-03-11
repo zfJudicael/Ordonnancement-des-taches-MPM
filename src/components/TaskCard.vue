7d8183609930ef00555c5a3621a6996969893fa4
Fanny Judicaël <fannyjudicael501@gmail.com>
2026-03-11 09:32:04 +0300

    style(TaskCard): rewrite the css with scss

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,9 +1,9 @@
 <template>
-    <Card style="margin-bottom: 5px;">
+    <Card class="taskCard">
         <template #content>
-          <div style="display: flex; justify-content: space-between; align-items: center;">
-            <p style="margin: 0; font-weight: bold; font-size: larger;"># {{ taskKey }}</p>
-            <div style="display: flex; justify-content: end; gap: 4px;">
+          <div class="header">
+            <p class="key"># {{ taskKey }}</p>
+            <div class="action">
               <Button icon="pi pi-pen-to-square" variant="text" severity="info" raised rounded aria-label="Edit" size="small" 
                @click="$emit('edit')"/>
               <Button icon="pi pi-trash" variant="text" severity="danger" raised rounded aria-label="Delete" size="small" 
@@ -13,7 +13,7 @@
           <p>Durée : {{ task.duration }}</p>
           <p>Marge de retard : {{ task.lateDate - task.earlyDate }}</p>
           <p style="margin-bottom: 2px;">Tâche(s) antérieure(s) :</p>
-          <div style="display: flex; flex-wrap: wrap; gap: 5px;">
+          <div class="previousTask_container">
             <Chip v-for="previousTask in task.previousTasks" :label="previousTask"/>
           </div>
         </template>
@@ -33,3 +33,27 @@ defineProps<{
 
 defineEmits(['edit', 'remove'])
 </script>
+
+<style lang="scss">
+.taskCard{
+  margin-bottom: 5px;
+
+  .header{
+    display: flex; justify-content: space-between; align-items: center;
+
+    .key{
+      margin: 0; font-weight: bold; font-size: larger;
+    }
+
+    .action{
+      display: flex; justify-content: end; gap: 4px;
+    }
+  }
+
+  .previousTask_container{
+    display: flex; flex-wrap: wrap; gap: 5px;
+  }
+
+
+}
+</style>
