11afa504ecd9d97e1545c7a2ffa6064cb2897d51
Fanny Judicaël <fannyjudicael501@gmail.com>
2026-03-17 20:37:04 +0300

    feat: convert images to webp format

--- src/const/staticFiles.ts
+++ src/const/staticFiles.ts
@@ -1,5 +1,5 @@
-import mpmGraph from '@/assets/images/01.jpeg';
-import mpmGraphCaptioned from '@/assets/images/01captioned.png';
+import mpmGraph from '@/assets/images/01.webp';
+import mpmGraphCaptioned from '@/assets/images/01captioned.webp';
 
 export const StaticFiles = {
     mpmGraph,
