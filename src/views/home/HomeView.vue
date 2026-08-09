0c4259c56707835134eeb16b1a818166fa7b54b9
Fanny Judicaël ZAFIMAROROKA <fannyjudicael501@gmail.com>
2026-08-09 07:02:10 +0300

    feat: add Lenis for enhanced scroll animation in HomeView

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
 import HeroSection from './sections/HeroSection.vue';
 import HistorySection from './sections/HistorySection.vue';
 import FeaturesSection from './sections/FeaturesSection.vue';
@@ -18,6 +19,31 @@ import HowItWorksSection from './sections/HowItWorksSection.vue';
 import GraphExplanation from './sections/GraphExplanation.vue';
 import CallToActionSection from './sections/CallToActionSection.vue';
 import Footer from './sections/Footer.vue';
+import Lenis from 'lenis'
+
+
+let lenis: Lenis | null = null
+let animationFrame: number
+onMounted(() => {
+  lenis = new Lenis({
+    duration: 1.8,
+    smoothWheel: true,
+  })
+
+  function raf(time: number) {
+    lenis?.raf(time)
+    animationFrame = requestAnimationFrame(raf)
+  }
+
+  animationFrame = requestAnimationFrame(raf)
+})
+
+onUnmounted(() => {
+  if (lenis) {
+    lenis.destroy()
+    lenis = null
+  }
+})
 </script>
 
 <style lang="scss">
