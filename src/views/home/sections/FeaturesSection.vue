40213224f9502513146b83b4a4f91e2d9c805499
Fanny Judicaël ZAFIMAROROKA <fannyjudicael501@gmail.com>
2026-08-13 12:01:30 +0300

    feat: add inViewOptions to display  animation once in FeaturesSection and HowItWorksSection

--- src/views/home/sections/FeaturesSection.vue
+++ src/views/home/sections/FeaturesSection.vue
@@ -11,6 +11,7 @@
           :initial="{ opacity: 0, y: 60 }"
           :whileInView="{ opacity: 1, y: 0 }"
           :transition="{ duration: 0.25, delay: index * 0.15 }"
+          :inViewOptions="{ once: true }"
         >
           <i :class="`pi ${feature.icon}`"></i>
           <h3>{{ feature.title }}</h3>
--- src/views/home/sections/HowItWorksSection.vue
+++ src/views/home/sections/HowItWorksSection.vue
@@ -10,6 +10,7 @@
                 :initial="{ opacity: 0, y: 40 }"
                 :whileInView="{ opacity: 1, y: 0 }"
                 :transition="{ duration: 0.25, delay: step.ordre * 0.15 }"
+                :inViewOptions="{ once: true }"
             >
                 <div class="order">
                     <p>{{ step.ordre }}</p>
